07be43c2f4cf15c199434b9114854c433021b7a6
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-04-27 01:25:15 +0900

    main

--- gotten-frontend-register/src/hooks/useLatLng.tsx
+++ gotten-frontend-register/src/hooks/useLatLng.tsx
@@ -4,11 +4,9 @@ const useLatLng = () => {
   const [location, setLocation] = useState<google.maps.LatLngLiteral | null>(
     null
   );
-  console.log(location);
   useEffect(() => {
     const Geolocation = navigator.geolocation;
     Geolocation.getCurrentPosition((position) => {
-      console.log("Hello");
       setLocation({
         lat: position.coords.latitude,
         lng: position.coords.longitude,
