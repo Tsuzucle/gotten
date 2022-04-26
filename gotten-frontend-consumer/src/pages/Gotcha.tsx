1941b6bb5edfc614d8bcfcbbb60108a54ea7bc8a
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-04-27 05:54:35 +0900

    main

--- gotten-frontend-consumer/src/pages/Gotcha.tsx
+++ gotten-frontend-consumer/src/pages/Gotcha.tsx
@@ -1,40 +1,73 @@
 import { useReactiveVar } from "@apollo/client";
 import qs from "qs";
 import { useCallback, useMemo, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { contractVar } from "../contract";
 import Column from "../components/Column";
 import { Button } from "antd-mobile";
-import { Typography } from "antd";
+import { Result, Typography } from "antd";
+import { css } from "@emotion/css";
 
 const { Text } = Typography;
 
+const exploreBaseUrl = "https://polygonscan.com/tx/";
+
 const Gotcha = () => {
   const contract = useReactiveVar(contractVar);
+  const navigate = useNavigate();
   const location = useLocation();
   const [error, setError] = useState("");
-  console.debug({ location });
+  const [exploreUrl, setExploreUrl] = useState("");
+
   const params = qs.parse(location.search, { ignoreQueryPrefix: true });
-  console.debug({ params });
+
   const { r: rand, p: password } = params;
   const key = `${rand}${password}`;
 
   const handleClickGotcha = useCallback(async () => {
     try {
-      const txHash = await contract!.gotcha(key);
+      const tx = await contract!.gotcha(key);
+      navigate(`/gotcha`);
+      setExploreUrl(`${exploreBaseUrl}${tx.hash}`);
     } catch (e) {
       console.log({ e });
       setError('"gotcha" しっぱいしました');
     }
   }, [contract]);
 
+  const handleClickGoExplore = useCallback(() => {
+    (window as any).location = exploreUrl;
+  }, [exploreUrl]);
+
   return (
-    <Column>
-      {key}
-      <Button onClick={handleClickGotcha}>Gotcha</Button>
-      {error && <Text type="danger">{error}</Text>}
+    <Column className={styles.root}>
+      {exploreUrl ? (
+        <Result
+          status="success"
+          title="Successfully Gotcha"
+          extra={[
+            <Button onClick={handleClickGoExplore}>Go To Explore</Button>,
+          ]}
+        />
+      ) : error ? (
+        <Text type="danger">{error}</Text>
+      ) : (
+        <Button color="success" onClick={handleClickGotcha}>
+          Gotcha
+        </Button>
+      )}
     </Column>
   );
 };
 
+const styles = {
+  root: css`
+    padding: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+  `,
+};
 export default Gotcha;
